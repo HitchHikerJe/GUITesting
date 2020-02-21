c84f107b93e8c9b9b51ee09a2afc7a8cfca029cb
Jackson Ellenberg <jjellenberg12@gmail.com>
2020-02-20 23:21:52 -0500

    adding drawings

--- GUITesting/src/Game.java
+++ GUITesting/src/Game.java
@@ -4,7 +4,13 @@ import java.awt.*;
 public class Game extends JPanel{
 	
 	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2 = (Graphics2D) g;
 		
+		//you can find a calculator for this online
+		g2.setColor(Color.getHSBColor(.5f, .75f, .8f));
+		
+		g2.fillOval(0, 0, 20, 20);
 	}
 	
 	public static void main(String[] args) {
@@ -15,6 +21,7 @@ public class Game extends JPanel{
 		frame.setVisible(true);
 		
 		Game game = new Game();
+		game.setBackground(Color.DARK_GRAY);
 		frame.add(game);
 		
 	}
