07f1854c09226fcce3af98d51439bbf8bb401732
Jackson Ellenberg <jjellenberg12@gmail.com>
2020-02-24 18:32:57 -0500

    I shuffled the pieces and now it works

--- GUITesting/src/main/Game.java
+++ GUITesting/src/main/Game.java
@@ -14,51 +14,51 @@ public class Game extends JPanel{
 	public Game() {
 		setBackground(Color.DARK_GRAY);
 		ball = new Ball(this);
-//		paddle = new Paddle(this);
-//		addKeyListener(new KeyListener() {
-//
-//			@Override
-//			public void keyTyped(KeyEvent e) {}
-//
-//			@Override
-//			public void keyPressed(KeyEvent e) {
-//				paddle.keyPressed(e);
-//			}
-//
-//			@Override
-//			public void keyReleased(KeyEvent e) {
-//				paddle.keyReleased(e);
-//			}
-//		});
-//		setFocusable(true);
+		paddle = new Paddle(this);
+		addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				paddle.keyPressed(e);
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				paddle.keyReleased(e);
+			}
+		});
+		setFocusable(true);
 	}
 	
 	public void paint(Graphics g) {
 		super.paint(g);
 		Graphics2D g2 = (Graphics2D) g;
 		ball.paint(g2);
-//		paddle.paint(g2);
+		paddle.paint(g2);
 	}
 	
 	public void move() {
 		ball.move();
-//		paddle.move();
+		paddle.move();
 	}
 	
 	public static void main(String[] args) throws InterruptedException{
 		JFrame frame = new JFrame("Many Sprites");
-		frame.setSize(300,200);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		
 		Game game = new Game();
 		frame.add(game);
 		
-//		while(true) {
-//			game.move();
-//			game.repaint();
-//			Thread.sleep(10);
-//		}
+		frame.setSize(300,200);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		
+		while(true) {
+			game.move();
+			game.repaint();
+			Thread.sleep(10);
+		}
 	}
 }
